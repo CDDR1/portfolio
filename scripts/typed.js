7f44f70c4ef6855cf5b532b223ae9b8349d1b45a
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-05-22 00:05:33 -0400

    Adding typed.js library CDN and configuration

---
+++ scripts/typed.js
@@ -0,0 +1,9 @@
+// import Typed from 'typed.js';
+
+const typed = new Typed("#typed", {
+    strings: ["Software Engineer", "Web Developer", "Programmer", "JavaScript Enthusiast"],
+    typeSpeed: 65,
+    backSpeed: 55,
+    backDelay: 1250,
+    loop: true
+});
