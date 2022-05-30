731d5f84566074f903d7c74907abdb6f52faf35d
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-05-30 14:22:41 -0400

    Changing nav

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -2,18 +2,14 @@
 const menu = document.querySelector(".navbar__menu");
 const closeMenuBtn = document.querySelector(".collapsable__close-btn");
 const nav = document.querySelector(".collapsable");
-const navLinks = document.querySelectorAll(".collapsable__link");
 const body = document.querySelector(".page");
 
-const showNav = () => {
-    nav.classList.remove("hidden");
-    body.style.overflowY = "hidden";
+const toggleNav = () => {
+    nav.classList.toggle("hidden");
+    body.classList.toggle("avoid-scroll");
+    menu.classList.toggle("hide-menu");
+    closeMenuBtn.classList.toggle("hidden");
 };
 
-const collapseNav = () => {
-    nav.classList.add("hidden");body.style.overflowY = "auto";
-};
-
-menu.addEventListener("click", showNav);
-closeMenuBtn.addEventListener("click", collapseNav);
-navLinks.forEach((link) => link.addEventListener("click", collapseNav));
+menu.addEventListener("click", toggleNav);
+closeMenuBtn.addEventListener("click", toggleNav);
