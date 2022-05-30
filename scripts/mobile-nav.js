934e04f4f821b3b9af4890d2f738400b029f4421
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-05-30 13:30:36 -0400

    Done with responsive styles

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,14 +1,17 @@
+// COLLAPSABLE NAV
 const menu = document.querySelector(".navbar__menu");
 const closeMenuBtn = document.querySelector(".collapsable__close-btn");
 const nav = document.querySelector(".collapsable");
 const navLinks = document.querySelectorAll(".collapsable__link");
+const body = document.querySelector(".page");
 
 const showNav = () => {
     nav.classList.remove("hidden");
+    body.style.overflowY = "hidden";
 };
 
 const collapseNav = () => {
-    nav.classList.add("hidden");
+    nav.classList.add("hidden");body.style.overflowY = "auto";
 };
 
 menu.addEventListener("click", showNav);
